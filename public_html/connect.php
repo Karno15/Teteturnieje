a468d664e190f8a5bc31b902d7bc4f211d0fe8f4
Karno15 <karnoplaypokemon@gmail.com>
2023-11-13 13:17:16 +0100

    Zmiana poświadczeń + CC
    Nowe poświadczenia dla nowego środowiska + Clean Code

--- public_html/connect.php
+++ public_html/connect.php
@@ -1,15 +1,20 @@
 <?php
-    $servername = "localhost";
-    $sa = "id20965121_sa";
-    $sa_pass = "x3t33dM.2023";
-    $dbname = "id20965121_teteturnieje";
 
-    // Create connection
+//to uncomment after publishing 
+// $servername = "localhost";
+// $sa = "id20965121_sa";
+//$sa_pass = "x3t33dM.2023";
+// $dbname = "id20965121_teteturnieje";
 
-    $conn = new mysqli($servername, $sa, $sa_pass, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }
- 
-?>
+
+$servername = "localhost";
+$sa = "root";
+$sa_pass = "";
+$dbname = "id20965121_teteturnieje";
+// Create connection
+
+$conn = new mysqli($servername, $sa, $sa_pass, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
--- public_html/index.php
+++ public_html/index.php
@@ -1,14 +1,15 @@
 <?php
-        
+
 session_start();
 
-            if(isset($_SESSION['info'])){
-            echo "<div class='info'>";
-            echo $_SESSION['info'];
-            echo "</div>";
-            unset($_SESSION['info']);
-            }
-            ?>
+if (isset($_SESSION['info'])) {
+    echo "<div class='info'>";
+    echo $_SESSION['info'];
+    echo "</div>";
+    unset($_SESSION['info']);
+}
+?>
+
 <head>
     <title>TTT-TeTeTurnieje</title>
     <link rel="icon" type="image/gif" href="/images/title.png">
@@ -19,60 +20,57 @@ session_start();
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="script.js"></script>
 </head>
+
 <body>
-           <div class="popup-overlay"></div> 
+    <div class="popup-overlay"></div>
     <div id="main-container">
 
         <div id='head'>
             <span>TETETURNIEJE</span>
-        </div> 
+        </div>
         <div id='content'>
             <div class='startpopup'>
-           <span id='titlejoin'>DOŁĄCZ DO TURNIEJU</span>
-            <div id='definput'>
+                <span id='titlejoin'>DOŁĄCZ DO TURNIEJU</span>
+                <div id='definput'>
                     Wpisz nickname:
                     <form action="join.php" method="post">
-                    <input type="text" class="inputy" name="login" maxlength="12" required>
+                        <input type="text" class="inputy" name="login" maxlength="12" required>
                 </div>
                 <div id='definput'>
                     Wpisz kod:
-                </br>
-                <input type="text" i class="inputy" name="gamecode" pattern="[0-9]{4}" maxlength="4" required>
+                    </br>
+                    <input type="text" i class="inputy" name="gamecode" pattern="[0-9]{4}" maxlength="4" required>
                 </div>
                 <button type='submit' class='codeconfrim'>Zatwierdź</button>
                 </form>
             </div><br>
-            <button class="button-85"
-            <?php
-            if(isset($_SESSION['userid']))
-                echo "id='host'";
-            else
-                echo "id='login'";
-            ?>
-             >Hostuj turniej</button>
-        </div> 
-            <div>
-            </div>
+            <button class="button-85" <?php
+                                        if (isset($_SESSION['userid']))
+                                            echo "id='host'";
+                                        else
+                                            echo "id='login'";
+                                        ?>>Hostuj turniej</button>
+        </div>
+        <div>
+        </div>
     </div>
- <div id='popup'>                <button id='closeButton' class='codeconfrim' >Powrót</button><br>
- LOGOWANIE 
+    <div id='popup'> <button id='closeButton' class='codeconfrim'>Powrót</button><br>
+        LOGOWANIE
         <br>
-                            <form action="login.php" method="post">
-                                Login:<br>
-                    <input type="text" name="login" class='inputlogin' maxlength="12" required>
+        <form action="login.php" method="post">
+            Login:<br>
+            <input type="text" name="login" class='inputlogin' maxlength="12" required>
 
-                <div id='definput'>
-                   Hasło:
+            <div id='definput'>
+                Hasło:
                 </br>
                 <input type="password" name="pass" class='inputlogin' required>
-                </div>
-                
-                <button type='submit' class='codeconfrim'>Loguj</button>
-                </form>
+            </div>
+
+            <button type='submit' class='codeconfrim'>Loguj</button>
+        </form>
 
     </div><br>
     <div id='footer'>Made by @karkarno</div>
 
 </body>
-
-
