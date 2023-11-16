0dd0e0940aa0cab005469389bd27e01b61fdb219
Karno15 <karnoplaypokemon@gmail.com>
2023-11-16 19:27:04 +0100

    buzz sound(test) + CC

--- public_html/joined.php
+++ public_html/joined.php
@@ -17,19 +17,16 @@ if (!isset($_SESSION['username'])) {
 
 require('connect.php');
 
-if (isset($_SESSION['info'])) {
-    echo "<div class='info'>";
-    echo $_SESSION['info'];
-    echo "</div>";
-    unset($_SESSION['info']);
-}
+$turniejid = $_SESSION['TurniejId'];
 
 ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 <script>
     $(document).ready(function() {
-        $('#participantsInfo').html('<div class="loading-spinner"></div>Loading...');
+        var buzzsfx = new Audio("sounds/buzz.wav");
 
+        $('#participantsInfo').html('<div class="loading-spinner"></div>Loading...');
+        var username = <?php echo isset($_SESSION['username']) ? json_encode($_SESSION['username']) : 'null'; ?>;
         function checkTournamentStatus() {
 
             $.ajax({
@@ -46,12 +43,13 @@ if (isset($_SESSION['info'])) {
 
                     // Wyświetl listę uczestników
                     var participantsList = '<table class="users">';
+                    
 
                     for (var i = 0; i < response.participants.length; i++) {
                         participantsList += '<tr><td>';
+                        
 
-                        var username = $("data#username").attr("value");
-
+                        
                         if (response.participants[i].Login == username) {
                             participantsList += '<b><font color="blue">' + response.participants[i].Login + "</font></b></td>"
                         } else {
@@ -66,50 +64,49 @@ if (isset($_SESSION['info'])) {
                         '</b><br>Rzule: ' + participantsList);
 
 
-                    if (status == 'R') {
+                    if (status == 'P') {
                         $('.startpopup').html('<button href="#" id="buzzer">BUZZ</button>');
                         $('#startform').hide();
 
-                        //                 $.ajax({
-                        //     url: 'quest.php',
-                        //     type: 'GET',
-                        //     dataType: 'json',
-                        //     success: function(quests) {
-                        //         console.log(quests);
-                        //         // Tutaj możesz przetwarzać dane zwrócone z quest.php
-                        //         var PytId = quests.PytId;
-                        //         var Quest = quests.Quest;
-                        //         var Category = quests.Category;
-                        //         var TypeId = quests.TypeId;
-                        //         var whoFirst = quests.whoFirst;
-                        //         var Rewards = quests.Rewards;
-                        //         var pozycje = quests.pozycje;
+                        $.ajax({
+                            url: 'quest.php',
+                            type: 'GET',
+                            dataType: 'json',
+                            success: function(quests) {
+                                console.log(quests);
+                                // Tutaj możesz przetwarzać dane zwrócone z quest.php
+                                var PytId = quests.PytId;
+                                var Quest = quests.Quest;
+                                var Category = quests.Category;
+                                var TypeId = quests.TypeId;
+                                var whoFirst = quests.whoFirst;
+                                var Rewards = quests.Rewards;
+                                var pozycje = quests.pozycje;
 
-                        //         $('.startpopup').html('Kategoria: '+Category +'<br>Punkty: '+Rewards +'<BR><span id="quest">' + Quest 
-                        //         +"</span><BR><div class='quest-options' id='questOptionsContainer'>"+'</div>');
+                                $('.startpopup').append('Kategoria: ' + Category + '<br>Punkty: ' + Rewards + '<BR><span id="quest">' + Quest +
+                                    "</span><BR><div class='quest-options' id='questOptionsContainer'>" + '</div>');
 
-                        //         wyswietlPozycje(pozycje);
+                                wyswietlPozycje(pozycje);
 
-                        //         $('#statusInfo').text('Status turnieju: ' + status);
+                                $('#statusInfo').text('Status turnieju: ' + status);
 
-                        //             // Wyświetl listę uczestników
-                        //             var participantsList = '<table class="users">';
+                                // Wyświetl listę uczestników
+                                var participantsList = '<table class="users">';
 
-                        //           for (var i = 0; i < response.participants.length; i++) {
-                        //     participantsList += '<tr><td><b>' + response.participants[i].Login +"</td><td></b> Wynik: <span class='score-edit' contenteditable='true' data-login='" + response.participants[i].Login + "'>" + response.participants[i].CurrentScore + '</span></td></tr>';
+                                for (var i = 0; i < response.participants.length; i++) {
+                                    participantsList += '<tr><td><b>' + response.participants[i].Login + "</td><td></b> Wynik: <span class='score-edit' contenteditable='true' data-login='" + response.participants[i].Login + "'>" + response.participants[i].CurrentScore + '</span></td></tr>';
 
-                        //            }
-                        //             participantsList += '</table>';
+                                }
+                                participantsList += '</table>';
+                                $('#participantsInfo').html('Organizator:<b> ' + response.creator +
+                                    '</b><br>Rzule: ' + participantsList);
 
-                        //             $('#participantsInfo').html('Organizator:<b> '+response.creator+
-                        //             '</b><br>Rzule: ' + participantsList);
-
-                        //     },
-                        //     error: function() {
-                        //         $('#statusInfo').text('Błąd podczas pobierania pytań.');
-                        //         $('#participantsInfo').html('Error');
-                        //     }
-                        // });
+                            },
+                            error: function() {
+                                $('#statusInfo').text('Błąd podczas pobierania pytań.');
+                                $('#participantsInfo').html('Error');
+                            }
+                        });
 
                     }
                 },
@@ -150,6 +147,7 @@ if (isset($_SESSION['info'])) {
 
         function buzz() {
             if (!pressed) {
+                buzzsfx.play();
                 $('#buzzer').prop('disabled', true);
                 pressed = true;
                 $('#buzzer').css("background", "gray");
@@ -161,7 +159,7 @@ if (isset($_SESSION['info'])) {
 
                 // AJAX request to insert a record into the 'buzzes' table
                 $.ajax({
-                    url: 'buzz.php', // Adjust the URL to your server-side script
+                    url: 'buzz.php',
                     type: 'POST',
                     data: {
                         userId: userId,
@@ -223,33 +221,28 @@ if (isset($_SESSION['info'])) {
             </div>
             <span id='turniej'>
                 <?php
-                $turniejid = $_SESSION['TurniejId'];
+                
 
                 if (isset($_SESSION['leader']) && $turniejid == $_SESSION['leader']) {
                     echo '<form method="post" id="startform">';
                     echo '<button id="start" name="start" class="button-85" type="submit" margin-top="0px">START</button>';
                     echo '</form>';
-                }
+                    if (isset($_POST['start'])) {
+                        // Wykonaj zapytanie do bazy danych, aby zaktualizować kod turnieju
+                        $sql = "UPDATE turnieje SET Status='P' WHERE TurniejId = $turniejid";
 
-                if (isset($_POST['start'])) {
-                    // Wykonaj zapytanie do bazy danych, aby zaktualizować kod turnieju
-                    $sql = "UPDATE turnieje SET Status='R' WHERE TurniejId = $turniejid";
-
-                    if ($conn->query($sql) === TRUE) {
-                        $_SESSION['info'] = "Success!";
-                    } else {
-                        $_SESSION['info'] = "Błąd!";
-                        //for debbuging echo . $conn->error;
+                        if ($conn->query($sql) === TRUE) {
+                            $_SESSION['info'] = "Success!";
+                        } else {
+                            $_SESSION['info'] = "Błąd!";
+                            //for debbuging echo . $conn->error;
+                        }
                     }
                 }
+
+
                 ?>
             </span>
-            <?php
-            echo '<data id="username" value=' . $_SESSION['username'] . '></data>';
-
-
-            $conn->close();
-            ?>
 
 
             <div id="statusInfo"></div>
