3c5ebaa3b294467ae3c65a5eec1c86b9a2ac6603
Karno15 <karnoplaypokemon@gmail.com>
2023-11-26 15:10:46 +0100

    Deploy on prod

--- connect.php
+++ connect.php
@@ -1,16 +1,17 @@
 <?php
 
-//to uncomment after publishing 
-// $servername = "localhost";
-// $sa = "id20965121_sa";
-//$sa_pass = "x3t33dM.2023";
-// $dbname = "id20965121_teteturnieje";
+
+//$servername = "localhost";
+//$sa = "root";
+//$sa_pass = "";
+//$dbname = "id20965121_teteturnieje";
 
 
+//FOR PROD:
 $servername = "localhost";
-$sa = "root";
-$sa_pass = "";
-$dbname = "id20965121_teteturnieje";
+$sa = "u843275928_sa";
+$sa_pass = "x3t33dM.2023";
+$dbname = "u843275928_teteturnieje";
 // Create connection
 
 $conn = new mysqli($servername, $sa, $sa_pass, $dbname);
