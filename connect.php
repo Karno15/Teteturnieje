296030a72550b68e13739b8a1f24e8618b16412e
Karno15 <karnoplaypokemon@gmail.com>
2024-03-25 13:03:39 +0100

    Revert "qc"
    This reverts commit 0e2e05b44246d6e2bb4d1e01ff6ab7a1b025d616.

--- connect.php
+++ connect.php
@@ -1,3 +1,18 @@
 <?php
-//hidden
 
+//to uncomment after publishing 
+// $servername = "localhost";
+// $sa = "id20965121_sa";
+//$sa_pass = "x3t33dM.2023";
+// $dbname = "id20965121_teteturnieje";
+
+
+$servername = "localhost";
+$sa = "root";
+$sa_pass = "";
+$dbname = "id20965121_teteturnieje";
+
+$conn = new mysqli($servername, $sa, $sa_pass, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
