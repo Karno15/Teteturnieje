8ed64263aa86c29bccb19bd975a5b0a58d666c79
Karno15 <121441345+Karno15@users.noreply.github.com>
2024-10-01 18:53:23 +0200

    Change visibility

--- connect.php
+++ connect.php
@@ -1,12 +1,5 @@
 <?php
 
-//to uncomment after publishing 
-// $servername = "localhost";
-// $sa = "id20965121_sa";
-//$sa_pass = "x3t33dM.2023";
-// $dbname = "id20965121_teteturnieje";
-
-
 $servername = "localhost";
 $sa = "root";
 $sa_pass = "";
